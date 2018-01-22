52a1c05c6ffae8d3270e367dae954a3baa5802ed
Frederick Munoz <frederickmunoz@fredericks-macbook-pro.local>
2018-01-22 17:44:35 -0600

    Initial Commit

---
+++ Velocimetro de Carro Digital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Velocimetro de Carro Digital
+//
+//  Created by Frederick Munoz on 1/22/18.
+//  Copyright © 2018 Frederick Munoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
